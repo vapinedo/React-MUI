84066d9bc5b6604151b53cb16ca87480a77f3a8e
Victor Pinedo <victorpinedomartinez@gmail.com>
2023-07-18 10:39:46 -0500

    updated app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import './App.css';
-import MuiSelect from './components/MuiSelect';
+import MuiSwitch from './components/MuiSwitch';
+// import MuiCheckbox from './components/MuiCheckbox';
+// import MuiRadioButton from './components/MuiRadioButton';
+// import MuiSelect from  './components/MuiSelect';
 // import MuiTextField from './components/MuiTextField';
 // import MuiButton from './components/MuiButton';
 // import MuiTypography fzrom './components/MuiTypography';
@@ -10,7 +13,10 @@ function App() {
       {/* <MuiTypography /> */}
       {/* <MuiButton /> */}
       {/* <MuiTextField /> */}
-      <MuiSelect />
+      {/* <MuiSelect /> */}
+      {/* <MuiRadioButton /> */}
+      {/* <MuiCheckbox /> */}
+      <MuiSwitch />
     </div>
   );
 }
